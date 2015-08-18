cabe8440a86a13996b8749449b3d2448b8d88c47
unknown <siwa.m@hotmail.com>
2015-08-18 13:47:33 +0700

    run1

--- app/src/main/java/pbru/mead/aoy/easytraffic/MainActivity.java
+++ app/src/main/java/pbru/mead/aoy/easytraffic/MainActivity.java
@@ -49,6 +49,21 @@ public class MainActivity extends ActionBarActivity {
         strTitle[17] = "จำกัดความเร็ว";
         strTitle[18] = "จำกัดความกว้าง";
         strTitle[19] = "จำกัดความสูง";
+
+        int [] intImage={R.drawable.traffic_01,R.drawable.traffic_02,
+                R.drawable.traffic_03,R.drawable.traffic_04,
+                R.drawable.traffic_05,R.drawable.traffic_06,
+                R.drawable.traffic_07,R.drawable.traffic_08,
+                R.drawable.traffic_09,R.drawable.traffic_10,
+                R.drawable.traffic_11,R.drawable.traffic_12,
+                R.drawable.traffic_13,R.drawable.traffic_14,
+                R.drawable.traffic_15,R.drawable.traffic_16,
+                R.drawable.traffic_17,R.drawable.traffic_18,
+                R.drawable.traffic_19,R.drawable.traffic_20,        };
+
+        Myadater objMyAdater = new Myadater(MainActivity.this, intImage, strTitle);
+        trafficListView.setAdapter(objMyAdater);
+
     }
 
 
--- app/src/main/java/pbru/mead/aoy/easytraffic/Myadater.java
+++ app/src/main/java/pbru/mead/aoy/easytraffic/Myadater.java
@@ -46,13 +46,13 @@ public class Myadater extends BaseAdapter{
     public View getView(int i, View view, ViewGroup viewGroup) {
 
         LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view1 = objLayoutInflater.inflate(R.layout.listview_traffic,viewGroup);
+        View view1 = objLayoutInflater.inflate(R.layout.listview_traffic,viewGroup, false);
         // show Image
         ImageView iconImageView = (ImageView) view1.findViewById(R.id.imvicon);
         iconImageView.setImageResource(iconInts[i]);
 
         // show Text
-        TextView titleTextView = (TextView) view1.findViewById(R.id.txtShowTitle);
+        TextView titleTextView = (TextView) view1.findViewById(R.id.textView);
         titleTextView.setText(titleStrings[i]);
 
         return view1;
